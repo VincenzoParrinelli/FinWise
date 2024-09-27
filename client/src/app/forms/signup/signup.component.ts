478c2757a2566ad2ffb9c8c6ddcc13fed0e1823e
Vincenzo Parrinelli <vincenzoparrinelli01@gmail.com>
2024-09-27 09:48:56 +0200

    feat(Signup): client side form validation

--- client/src/app/forms/signup/signup.component.ts
+++ client/src/app/forms/signup/signup.component.ts
@@ -1,10 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 import { MainLayoutComponent } from '../../shared/layouts/main/main.component';
 import { EyeOpenComponent } from '../../svg/eye-open/eye-open.component';
@@ -27,28 +31,121 @@ import { RouterService } from '../../router.service';
 export class SignupComponent {
   signupForm = new FormGroup({
     fullName: new FormControl('', {
-      validators: [Validators.required],
+      validators: [Validators.required, Validators.maxLength(40)],
     }),
     email: new FormControl('', {
-      validators: [Validators.email, Validators.required],
+      validators: [Validators.required, Validators.email],
     }),
     phone: new FormControl('', {
       validators: [Validators.required],
     }),
     dateOfBirth: new FormControl('', {
-      validators: [Validators.required],
+      validators: [Validators.required, this.dateValidator()],
     }),
     password: new FormControl('', {
-      validators: [Validators.required],
+      validators: [Validators.required, Validators.minLength(8)],
     }),
     confirmPassword: new FormControl('', {
-      validators: [Validators.required],
+      validators: [Validators.required, this.passwordMatchValidator()],
     }),
   });
+
+  private dateValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!this.signupForm) return null;
+
+      const dateOfBirth = new Date(control.value);
+
+      return isNaN(dateOfBirth.getTime()) ? { invalidDate: true } : null;
+    };
+  }
+
+  private passwordMatchValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!this.signupForm) return null;
+
+      const password = this.signupForm.value.password;
+      const confirmPassword = control.value;
+
+      return password !== confirmPassword ? { passwordMismatch: true } : null;
+    };
+  }
+
+  get isFullNameRequired() {
+    return (
+      this.signupForm.controls.fullName.hasError('required') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isFullNameMaxLengthExceeded() {
+    return (
+      this.signupForm.controls.fullName.hasError('maxlength') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isEmailRequired() {
+    return (
+      this.signupForm.controls.email.hasError('required') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isPhoneRequired() {
+    return (
+      this.signupForm.controls.phone.hasError('required') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isDateOfBirthRequired() {
+    return (
+      this.signupForm.controls.dateOfBirth.hasError('required') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isPasswordRequired() {
+    return (
+      this.signupForm.controls.password.hasError('required') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isConfirmPasswordRequired() {
+    return (
+      this.signupForm.controls.confirmPassword.hasError('required') &&
+      this.formIsSubmitted()
+    );
+  }
+
+  get isConfirmPasswordMismatched() {
+    return (
+      this.signupForm.controls.confirmPassword.hasError('passwordMismatch') &&
+      this.formIsSubmitted()
+    );
+  }
 
   isPasswordVisible: boolean = false;
   isConfirmPasswordVisible: boolean = false;
   routerService = inject(RouterService);
+  private httpClient = inject(HttpClient);
+  private formIsSubmitted = signal<boolean>(false);
 
-  onSubmit() {}
+  onSubmit() {
+    this.formIsSubmitted.set(true);
+
+    if (!this.signupForm.valid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+
+    this.httpClient
+      .post(
+        'http://localhost:5000/api/users/create-user',
+        this.signupForm.value
+      )
+      .subscribe();
+  }
 }
