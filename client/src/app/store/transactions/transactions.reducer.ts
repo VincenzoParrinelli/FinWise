dfd692e6e9ca75ac8733b2fdb6a93254ce1ed381
Vincenzo Parrinelli <vincenzoparrinelli01@gmail.com>
2024-11-07 23:22:33 +0100

    perf(transactionsReducer): sorting new transaction into list + binary search inserting and sorting new paginated transactions avoiding duplicates after new transactions had been inserted

--- client/src/app/store/transactions/transactions.reducer.ts
+++ client/src/app/store/transactions/transactions.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { TransactionsState } from './transactions.model';
+import { Transaction, TransactionsState } from './transactions.model';
 import * as TransactionsActions from './transactions.actions';
 
 export const initialState: TransactionsState = {
@@ -21,10 +21,10 @@ export const transactionsReducer = createReducer(
       totalIncome: transactionsWithTotals.totalIncome,
       totalExpenses: transactionsWithTotals.totalExpenses,
       totalDocuments: transactionsWithTotals.totalDocuments,
-      transactions: [
-        ...state.transactions,
-        ...transactionsWithTotals.transactions,
-      ],
+      transactions: removeDuplicatesAndSort(
+        state.transactions,
+        transactionsWithTotals.transactions
+      ),
     })
   ),
   on(TransactionsActions.createTransaction, (state) => ({
@@ -34,7 +34,66 @@ export const transactionsReducer = createReducer(
     TransactionsActions.createTransactionSuccess,
     (state, { transaction }) => ({
       ...state,
-      transactions: [...state.transactions, transaction],
+      totalBalance: state.totalBalance + transaction.amount,
+      totalIncome:
+        transaction.amount > 0
+          ? state.totalIncome + transaction.amount
+          : state.totalIncome,
+      totalExpenses:
+        transaction.amount < 0
+          ? state.totalIncome + transaction.amount
+          : state.totalExpenses,
+      totalDocuments: state.totalDocuments + 1,
+
+      transactions: [...state.transactions, transaction].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      ),
     })
   )
 );
+
+const removeDuplicatesAndSort = (
+  transactions: Transaction[],
+  newTransactions: Transaction[]
+): Transaction[] => {
+  const map = new Map<string, Transaction>();
+
+  transactions.forEach((transaction) => {
+    map.set(transaction._id!, transaction);
+  });
+
+  newTransactions.forEach((transaction) => {
+    map.set(transaction._id!, transaction);
+  });
+
+  const uniqueTransactions = Array.from(map.values());
+  const result: Transaction[] = [];
+
+  uniqueTransactions.forEach((transaction) => {
+    const index = binarySearch(result, new Date(transaction.date));
+
+    result.splice(index, 0, transaction);
+  });
+
+  return result;
+};
+
+const binarySearch = (arr: Transaction[], date: Date) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midDate = new Date(arr[mid].date).getTime();
+
+    if (midDate === date.getTime()) {
+      return mid;
+    } else if (midDate > date.getTime()) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};
