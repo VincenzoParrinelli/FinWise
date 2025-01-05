a697c34bf2a78ed8511baa04704cf55bba07fd96
Vincenzo Parrinelli <vincenzoparrinelli01@gmail.com>
2025-01-05 08:54:56 +0100

    fix(svgs): missing fill and stroke attr into categories svg template

--- client/src/app/svg/silverware/silverware.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-silverware',
-  standalone: true,
-  imports: [],
-  templateUrl: './silverware.component.svg',
-})
-export class SilverwareComponent {}
